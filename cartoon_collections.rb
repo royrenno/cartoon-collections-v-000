9427d1e8475c0db71e9fab868092eff07fc69f0e
roy renno <royrenno@yahoo.com>
2019-02-10 22:21:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,14 @@ def summon_captain_planet(planeteer_calls)
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words,assorted_words)
+  short_words = ["puff","go","two"]
+  assorted_words = ["two","go","industrious","bop"]
+  word = 4
+  if word > 4
+    false
+  elsif word =<4
+    return true
 end
 
 def find_the_cheese# code an argument here
