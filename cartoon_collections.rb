719be77bff462135c38d8e8826ddd73fc51372c3
roy renno <royrenno@yahoo.com>
2019-02-10 22:53:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,50 +6,29 @@ puts "#{index + 1}. #{name};"}
 end
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-
 def summon_captain_planet(planeteer_calls)
-
 planeteer_calls.map {|calls| calls.capitalize + "!" }
-
 end
 
 short_words = ["puff", "go", "two"]
-
 assorted_words = ["two", "go", "industrious", "bop"]
-
 def long_planeteer_calls(assorted_words)
-
 long_words = []
-
 assorted_words.each do |word|
-
 if word.length > 4
-
 long_words << word
-
 end
-
 end
-
 if long_words.length > 0
-
 return true
-
 else
-
 return false
-
+end
 end
 
-end
 
-
-snacks_array = ["crackers", "gouda", "thyme"]
-
-def find_the_cheese(snacks_array)
-
-cheese = ["cheddar", "gouda", "camembert"]
-
-snacks_array.find { |snack| cheese.include?(snack) }
-
+snacks = ["crackers", "gouda", "thyme"]
+def find_the_cheese(snacks)
+soup = ["tomato soup","cheddar", "oyster crackers", "gouda"]
+snacks.find { |snack| cheese.include?(snack) }
 end
