61ce181eb4fb23012634570e130656974d7b8c0a
roy renno <royrenno@yahoo.com>
2019-02-10 22:41:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,32 +1,56 @@
-dwarves = ["Doc","Dopey","Bashful","Grumpy"]
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index {|name, index|}
-  puts "#{index + 1}. #{name;}"
+
+dwarves.each_with_index {|name, index|
+
+puts "#{index + 1}. #{name};"}
+
 end
 
-planeteer_calls = ["earth","wind","fire","water","heart"]
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
 def summon_captain_planet(planeteer_calls)
-  planeteer = planeteer_calls.map {|calls| calls.capitalize +"!"}
+
+planeteer_calls.map {|calls| calls.capitalize + "!" }
+
 end
 
-short_words = ["puff","go","two"]
-assorted_words = ["two","go","industrious","bop"]
+short_words = ["puff", "go", "two"]
 
-def long_planeteer_calls(short_words,assorted_words)
-  long_words = []
-  assorted_words.each do |word|
-    if word.length > 4
-      long_words << word
-    end
+assorted_words = ["two", "go", "industrious", "bop"]
+
+def long_planeteer_calls(assorted_words)
+
+long_words = []
+
+assorted_words.each do |word|
+
+if word.length > 4
+
+long_words << word
+
 end
+
+end
+
 if long_words.length > 0
-  return true
+
+return true
+
 else
-  return false
-end
+
+return false
+
 end
 
-def find_the_cheese
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.include?("cheddar")
+end
+
+
+snacks_array = ["crackers", "gouda", "thyme"]
+
+def find_the_cheese(snacks_array)
+
+cheese = ["cheddar", "gouda", "camembert"]
+
+snacks_array.find { |snack| cheese.include?(snack) }
+
 end
